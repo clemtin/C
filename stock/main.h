4f7c521485ce9d34a259166ae6791bac1e7f6a4b
clemtym <boillatclement@gmail.com>
2025-09-08 18:18:12 +0200

    organised

---
+++ stock/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+typedef struct {
+    int id;
+    char nom[50];
+    int quantite;
+    float prix_unitaire;
+}
+
+extern void init;
+extern void
+
+
+
+
+
+
+
+
+
+#endif
