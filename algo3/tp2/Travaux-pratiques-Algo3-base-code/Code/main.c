f690f92bf262e6ceb544e2e0a3aa27faaa4af910
clemtym <boillatclement@gmail.com>
2025-10-23 11:52:30 +0200

    tp2 modif stringtotokenqueue

--- algo3/tp2/Travaux-pratiques-Algo3-base-code/Code/main.c
+++ algo3/tp2/Travaux-pratiques-Algo3-base-code/Code/main.c
@@ -17,9 +17,16 @@
 void print_token(const void* e, void* user_param);
 void print_queue(FILE* f, Queue* q);
 
+
 /** 
  * Function to be written by students
  */
+
+
+bool issymbol(const char c);
+Queue* stringToTokenQueue(const char* expression);
+void computeExpressions(FILE* input);
+
 void computeExpressions(FILE* input) {
 	char *line = NULL;
 	size_t linecap = 0;
@@ -29,37 +36,54 @@ void computeExpressions(FILE* input) {
 		if(line[0]=='\n')
 			continue;
 		printf("Ìnput : %s",line);
+
+		Queue* infix = stringToTokenQueue(line);
+		printf("Inifx : ");
+		print_queue(stdout,infix);
 	}
+	
 }	
+
+
+
+bool isSymbol(char c) {
+    return (c == '&' || c == '+' || c == '-' || c == '*' ||
+            c == '\'' || c == '^' || c == '(' || c == ')');
+}
+
 
 Queue* stringToTokenQueue(const char* expression){
 	int cmpt = 0;
-	Queue* q=create_queue(void);
+	Queue* q=create_queue();
 	const char* curpos = expression;
-	while(curpos!='\0'){
+	while(*curpos!='\0'){
 		
-		while((curpos!=' ')||(curpos!='\n')){
-		curpos++; //  Le début de l’élément syntaxique devra etre désigné par curpos,
-		cmpt++; // 	la fin de l’élément par le nombre de caractere le composant.
-
-			if(isSymbol(curpos)&&(curpos!='\0')){
-				Token* t=create_token_from_string(curpos,1);
-				Queue_push(q,curpos);
-				curpos++;
-			}
-
-			
-			
-		
-			}
-
+		while((*curpos!=' ')||(*curpos!='\n')){
+			curpos++; //  Le début de l’élément syntaxique devra etre désigné par curpos,
+			if(*(curpos++)=='\0')
+				break;
 		}
-			return q;
+
+		if(isSymbol(*curpos)){
+			Token* to=create_token_from_string(curpos,1);
+			queue_push(q,to);
+			curpos++;
+			}
+				
+		else{
+			while(*curpos!='\0'){
+				
+				
+				}
+			}
+		}
+				
+		return q;
 	}
-bool isSymbol(char c){
-	return(((c=='&')||(c=='+')||(c=='-'))||(c=='*')||(c==''\'')||(c=='^')||(c=='(')||(c==')'));
-		
-}
+
+
+
+
 
 /** Main function for testing.
  * The main function expects one parameter that is the file where expressions to translate are
